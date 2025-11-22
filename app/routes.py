b30c245e2b8e32d8a60acccd359a4b45e700e63d
vega710 <laklaihicham.gl@gmail.com>
2025-11-22 22:04:40 +0100

    9adi date and time and if the user enter an non valid city name

--- app/routes.py
+++ app/routes.py
@@ -6,18 +6,39 @@ main = Blueprint('main',__name__)
 
 @main.route("/",methods=['GET','POST'])
 def get_weather():
-    city='rabat'
-    
+    data,img,icon_url=None,None,None
     if request.method=='POST':
         city=request.form.get("cityname")
-    api_key= app.config["OPENWEATHER_API_KEY"]
-    api_url=f"https://api.openweathermap.org/data/2.5/weather?q={city}&units=metric&appid={api_key}"
+        if city:
+            api_key= app.config["OPENWEATHER_API_KEY"]
+            api_url=f"https://api.openweathermap.org/data/2.5/weather?q={city}&units=metric&appid={api_key}"
+            response = requests.get(api_url)
+            response_state_code = response.status_code
+            if response.status_code==200:
+                data=response.json()
+                data["main"]["temp"]=round(data["main"]["temp"])
+                data["wind"]["speed"]= round(data["wind"]["speed"]*3.6,1)
 
-    response = requests.get(api_url)
-    data=response.json()
-    data["main"]["temp"]=round(data["main"]["temp"])
-    data["wind"]["speed"]= round(data["wind"]["speed"]*3.6,1)
-    icon = data["weather"][0]["icon"]
-    icon_url = f"https://openweathermap.org/img/wn/{icon}@2x.png"
+                weather_state=data["weather"][0]["main"]
+                if weather_state=='Clouds':
+                    img='clouds.png'
+                elif weather_state=='Drizzle':
+                    img='drizzle.png'
+                elif weather_state=='Rain':
+                    img='rain.png'
+                elif weather_state=='Snow':
+                    img='snow.png'
+                elif weather_state=='Clear':
+                    img='clear.png'
+                elif weather_state=='Mist':
+                    img='mist.png'
+                icon_url = f"../static/images/icons/{img}"
 
+            else:
+                data={"error":"City Not Found"}
+            
+
+        #icon = data["weather"][0]["icon"]
+        #icon_url = f"https://openweathermap.org/img/wn/{icon}@2x.png"
+            
     return render_template('mainpage.html',data=data,icon_url=icon_url)
