67e461f40cc783a8e19bf21b3915c94a0321dc9a
vega710 <laklaihicham.gl@gmail.com>
2025-11-23 14:21:43 +0100

    removing api key from config file

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 import os
 
 class Config():
-    OPENWEATHER_API_KEY = os.getenv("OPENWEATHER_API_KEY","214d19ef5ad16169fd6dc7e2b2e9ae28")
+    OPENWEATHER_API_KEY = os.getenv("OPENWEATHER_API_KEY")
