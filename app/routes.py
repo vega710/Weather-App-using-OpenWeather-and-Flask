72db92740f20e2c37df5fc3f41492c5796d60db3
vega710 <laklaihicham.gl@gmail.com>
2025-11-23 13:27:01 +0100

    adding sunsrise and sunset time + some css adjusting for better layout

--- app/routes.py
+++ app/routes.py
@@ -1,22 +1,24 @@
 from flask import Blueprint, render_template, request
 import requests
 from flask import current_app as app
+from datetime import datetime
 
 main = Blueprint('main',__name__)
 
 @main.route("/",methods=['GET','POST'])
 def get_weather():
-    data,img,icon_url=None,None,None
+    data,img,icon_url,forcast,sunrise,sunset=None,None,None,None,None,None
     if request.method=='POST':
         city=request.form.get("cityname")
         if city:
             api_key= app.config["OPENWEATHER_API_KEY"]
             api_url=f"https://api.openweathermap.org/data/2.5/weather?q={city}&units=metric&appid={api_key}"
             response = requests.get(api_url)
-            response_state_code = response.status_code
             if response.status_code==200:
                 data=response.json()
                 data["main"]["temp"]=round(data["main"]["temp"])
+                data["main"]["temp_min"]=round(data["main"]["temp_min"])
+                data["main"]["temp_max"]=round(data["main"]["temp_max"])
                 data["wind"]["speed"]= round(data["wind"]["speed"]*3.6,1)
 
                 weather_state=data["weather"][0]["main"]
@@ -38,7 +40,19 @@ def get_weather():
                 data={"error":"City Not Found"}
             
 
+            api_url=f"https://api.openweathermap.org/data/2.5/forecast?q={city}&cnt=1&units=metric&appid={api_key}"
+            response = requests.get(api_url)
+            if response.status_code==200:
+                forcast=response.json()
+                sunrise_utc_time = datetime.fromtimestamp(forcast["city"]["sunrise"])
+                sunrise = sunrise_utc_time.strftime("%H:%M")
+                sunset_utc_time = datetime.fromtimestamp(forcast["city"]["sunset"])
+                sunset = sunset_utc_time.strftime("%H:%M")
+                
+                
+            
+
         #icon = data["weather"][0]["icon"]
         #icon_url = f"https://openweathermap.org/img/wn/{icon}@2x.png"
             
-    return render_template('mainpage.html',data=data,icon_url=icon_url)
+    return render_template('mainpage.html',data=data,icon_url=icon_url,forcast=forcast,sunrise=sunrise,sunset=sunset)
